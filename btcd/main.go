6e4741faa4d29ca31b1f15ccb5979f47f04c4503
Charles Rosenbauer <charles.rosenbauer@gmail.com>
2018-12-03 20:43:01 -0600

    It's actually working now, more or less

--- btcd/main.go
+++ btcd/main.go
@@ -41,25 +41,43 @@ func getCPUUsage() (total, idle uint64) {
 
 func main() {
 	start:
-		cmd := exec.Command("/go/bin/btcd", "—txindex", "—datadir=pi/media/Untiled/chaindata")
+		args := []string{"--txindex", "--datadir=../../../../media/pi/Picard/chaindata"}
+		cmd := exec.Command("./btcd", args...)
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
+
+		for _, arg := range cmd.Args {
+			println(arg)
+		}
 
 		err := cmd.Start()
 		if err != nil {
 			log.Fatal(err)
 		}
 
+		i := 0
+		dropcount := 0		// How many seconds has cpu usage been down?
+		_, cpuaccum := getCPUUsage()
 		ticker := time.NewTicker(1 * time.Second)
 		for t := range ticker.C {
-			_, cpu := getCPUUsage()
-			if cpu <= 5 {
+			_, cpu   := getCPUUsage()
+			cpudiff  := cpu - cpuaccum
+			cpuaccum = cpu
+			if cpudiff >= 380 {
+				dropcount += 1
+			}
+
+			if dropcount >= 150 {
 				if err := cmd.Process.Kill(); err != nil {
 					log.Fatal(err)
 				}
 				time.Sleep(5 * time.Second) // Give time to btcd to shut down
 				goto start
 			}
-			println(t.String())
+			if (i % 30) == 0 {
+				println(t.String())
+				println("CPU USAGE:: ", (float32(cpudiff) / 4.0), "%")
+			}
+			i++
 		}
 }
